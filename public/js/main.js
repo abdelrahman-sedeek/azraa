2ee8bc06b2b74df7bce6d907082d7354dcf843f1
abdelrahman tolba sedeek <67134107+abdelrahman-sedeek@users.noreply.github.com>
2023-12-20 03:45:58 +0200

    .

---
+++ public/js/main.js
@@ -0,0 +1,162 @@
+// /////////////////////
+window.addEventListener("scroll", function () {
+    let nav = document.querySelector(".navbar");
+    nav.classList.toggle("fix", window.scrollY > 150);
+}) 
+//////////////////////////
+// ////////////////////////////////////////////
+var owl = $('#slider_1');
+owl.owlCarousel({
+    items:4, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    navigation: true,
+    autoplay: true,
+    smartSpeed: 800,
+    rtl:true,
+    navElement: "dots",
+    navText:[`<i class="fa-solid fa-chevron-right"></i>` , `<i class="fa-solid fa-chevron-left"></i>`],
+    paginationSpeed : 800,
+    dots: true,  
+    dotsEach:true,
+    responsive:{
+        0:{
+            items: 2,
+        },
+        600:{
+            items: 5,
+        },
+        1100:{
+            items: 7,
+        },
+        1200:{
+            items:8
+        }
+    }
+});
+// /////////
+
+// 
+// ////////////////////////////////////////////
+var owl = $('#slider_2');
+owl.owlCarousel({
+    items:4, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    navigation: true,
+    autoplay: true,
+    smartSpeed: 800,
+    rtl:true,
+    navElement: "dots",
+    navText:[`<i class="fa-solid fa-chevron-right"></i>` , `<i class="fa-solid fa-chevron-left"></i>`],
+    paginationSpeed : 800,
+    dots: true,  
+    dotsEach:true,
+    responsive:{
+        0:{
+            items: 2,
+        },
+        600:{
+            items: 3,
+        },
+        1100:{
+            items: 4,
+        },
+        1200:{
+            items:5
+        }
+    }
+});
+// /////////
+
+// ////////////////////////////////////////////
+var owl = $('#slider_3');
+owl.owlCarousel({
+    items:4, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    navigation: true,
+    autoplay: true,
+    smartSpeed: 800,
+    rtl:true,
+    navElement: "dots",
+    navText:[`<i class="fa-solid fa-chevron-right"></i>` , `<i class="fa-solid fa-chevron-left"></i>`],
+    paginationSpeed : 800,
+    dots: true,  
+    dotsEach:true,
+    responsive:{
+        0:{
+            items: 2,
+        },
+        600:{
+            items: 3,
+        },
+        1100:{
+            items: 4,
+        },
+        1200:{
+            items:5
+        }
+    }
+});
+// /////////
+
+
+
+
+
+
+window.addEventListener("scroll" , function () {
+    let scrollUp = document.querySelector(".scroll_up");
+    scrollUp.classList.toggle("scroll_transition", window.scrollY > 150);
+    scrollUp.classList.toggle("show_scroll", window.scrollY > 200)
+})
+    let scrollUp = document.querySelector(".scroll_up");
+
+scrollUp.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior:"smooth"
+    })
+})
+//////////////////////////
+
+let Login = document.getElementById("Login");
+let lost_pass = document.getElementById("lost_pass");
+let Git_Back = document.getElementById("Git_Back");
+
+lost_pass.addEventListener("click", function () {
+    Login.classList.add("lost_pass_container")
+})
+Git_Back.addEventListener("click", function () {
+    Login.classList.remove("lost_pass_container")
+})
+
+////////////////////////////
+let cart_open_tool = document.getElementById("cart_open_tool");
+let cart_sidbar = document.getElementById("cart_sidbar");
+let close_cart = document.getElementById("close_cart");
+
+cart_open_tool.addEventListener("click", function () {
+    cart_sidbar.classList.add("Show")
+})
+
+close_cart.addEventListener("click", function () {
+    cart_sidbar.classList.remove("Show")
+})
+// //////////////
+let Add_to_Cart_chose_container = document.getElementById("Add_to_Cart_chose_container");
+let close_chose = document.getElementById("close_chose");
+function addtoChose  () {
+let Add_to_Cart_chose_container = document.getElementById("Add_to_Cart_chose_container");
+    Add_to_Cart_chose_container.classList.add("Show_Cart_Chose")
+}
+close_chose.addEventListener("click", function () {
+    Add_to_Cart_chose_container.classList.remove("Show_Cart_Chose")
+})
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,185 @@
+@include('home.head')
+<!-- S1 home -->
+<section class="S1_home">
+    <div class="container">
+        <h1>ازرع للمنتجات</h1>
+        <p>وداعا ل غلاء الاسعار اسعار الجمله توصلك لحد بيتك</p>
+        <a href="./categories.php"> تسوق معنا </a>
+    </div>    
+    </section>
+    <!-- S2 home Categories-->
+    <section class="S2_home">
+    <div class="container">
+        <div class="top_sections">
+            <h3> التصنيفات </h3>
+    
+            <a href="./categories.php">شاهد المزيد</a>
+        </div>
+        <div class="owl-carousel owl-theme" id="slider_1">
+           
+            <div class="item">
+                <a href="./index.php">
+                    <img src="./img/catt_1.jpg" alt="">
+                    <h3>نظافه البيت</h3>
+                </a>
+            </div>
+    
+    
+            <div class="item">
+                <a href="./index.php">
+                    <img src="./img/catt_2.jpg" alt="">
+                    <h3>زيت و سمن</h3>
+                </a>
+            </div>
+    
+            <div class="item">
+                <a href="./index.php">
+                    <img src="./img/catt_3.jpg" alt="">
+                    <h3>مشروبات</h3>
+                </a>
+            </div>
+    
+            <div class="item">
+                <a href="./index.php">
+                    <img src="./img/catt_4.jpg" alt="">
+                    <h3>تسالي</h3>
+                </a>
+            </div>
+    
+            <div class="item">
+                <a href="./index.php">
+                    <img src="./img/catt_5.jpg" alt="">
+                    <h3>معلبات</h3>
+                </a>
+            </div>
+    
+    
+    
+        </div>
+    </div>
+    </section>
+    <!-- S3 home  Best offers-->
+    <section class="S4_home product">
+    <div class="container">
+        <div class="top_sections">
+            <h3> التصنيفات </h3>
+        
+            <a href="./categories.php">شاهد المزيد</a>
+        </div>
+        <div class="owl-carousel owl-theme" id="slider_2">
+        
+            <div class="item">
+                <div class="box">
+                    <img src="./img/offers_1.jpg" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                    <span> 10جم بدلا من <del>20جم</del> </span>
+                    <button onclick="addtoChose()" class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                </div>
+            </div>
+        
+        
+            <div class="item">
+                <div class="box">
+                    <img src="./img/offers_2.png" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                    <span> 20جم بدلا من <del>27جم</del> </span>
+                    <button onclick="addtoChose()" class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                </div>
+            </div>
+        
+            <div class="item">
+                <div class="box">
+                    <img src="./img/offers_3.png" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                    <span> 55جم بدلا من <del>60جم</del> </span>
+                    <button onclick="addtoChose()" class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                </div>
+            </div>
+        
+            <div class="item">
+                <div class="box">
+                    <img src="./img/offers_4.png" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                    <span> 55جم بدلا من <del>60جم</del> </span>
+                    <button onclick="addtoChose()" class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                </div>
+            </div>
+        
+            <div class="item">
+                <div class="box">
+                    <img src="./img/offers_5.png" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                    <span> 7جم بدلا من <del>10جم</del> </span>
+                    <button onclick="addtoChose()" class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                </div>
+            </div>
+        
+        
+        
+        </div>
+    </div>    
+    </section>
+    
+    
+    <!-- S3 home  New added-->
+    <section class="S5_home S4_home product">
+        <div class="container">
+        <div class="top_sections">
+            <h3> المضاف حديثا </h3>
+        
+            <a href="./categories.php">شاهد المزيد</a>
+        </div>
+            <div class="owl-carousel owl-theme" id="slider_3">
+    
+                <div class="item">
+                    <div class="box">
+                        <img src="./img/offers_1.jpg" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                        <span> 10جم بدلا من <del>20جم</del> </span>
+                        <button onclick="addtoChose()"   class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                    </div>
+                </div>
+    
+    
+                <div class="item">
+                    <div class="box">
+                        <img src="./img/offers_2.png" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                        <span> 20جم بدلا من <del>27جم</del> </span>
+                        <button onclick="addtoChose()"   class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <div class="box">
+                        <img src="./img/offers_3.png" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                        <span> 55جم بدلا من <del>60جم</del> </span>
+                        <button onclick="addtoChose()"   class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <div class="box">
+                        <img src="./img/offers_4.png" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                        <span> 55جم بدلا من <del>60جم</del> </span>
+                        <button onclick="addtoChose()"   class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                    </div>
+                </div>
+    
+                <div class="item">
+                    <div class="box">
+                        <img src="./img/offers_5.png" alt="">
+                    <a href="./single_product.php">1 كيلو سكر ابيض صافي</a>
+                        <span> 7جم بدلا من <del>10جم</del> </span>
+                        <button onclick="addtoChose()"  class="add-to-cart"> اضف الي العربه <i class="fa-solid fa-cart-shopping"></i> </button>
+                    </div>
+                </div>
+    
+    
+    
+            </div>
+        </div>
+    </section>
+    @include('home.footer')
