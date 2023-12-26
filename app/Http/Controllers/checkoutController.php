cd3866fb018a43ae8a531b4a705322b19fed29f0
abdelrahman tolba sedeek <67134107+abdelrahman-sedeek@users.noreply.github.com>
2023-12-26 22:15:03 +0200

    .

--- app/Http/Controllers/checkoutController.php
+++ app/Http/Controllers/checkoutController.php
@@ -15,46 +15,54 @@ class checkoutController extends Controller
         {
             return view('home.checkout');
         }
-    public function add(Request $request){
-        $data = $request->all();
-        $user_id = auth()->id();
-        $discount = $data['price'];
-        $total = $data['total'];
-    
-        $cartItems = Cart::where('user_id', $user_id)->get();
-     DB::beginTransaction();
-    
-        try {
-            foreach ($cartItems as $cartItem) {
-                $productId = $cartItem->main_pro_id;
-                $productBranchId = $cartItem->product_id;
-                $quantity = $cartItem->quantity;
-    
-                $product = Product::find($productId);
-                $productBranch = ProductBranch::find($productBranchId);
-    
-                $product->stock -= $quantity * $productBranch->measurement;
-                $product->save();
+        public function add(Request $request)
+        {
+            $data = $request->all();
+            $user_id = auth()->id();
+            $discount = $data['price'];
+            $total = $data['total'];
+        
+            $cartItems = Cart::where('user_id', $user_id)->get();
+            DB::beginTransaction();
+        
+            try {
+                foreach ($cartItems as $cartItem) {
+                    $productId = $cartItem->main_pro_id;
+                    $productBranchId = $cartItem->product_id;
+                    $quantity = $cartItem->quantity;
+        
+                    $product = Product::find($productId);
+                    $productBranch = ProductBranch::find($productBranchId);
+        
+                    // Check if there is enough stock
+                    if ($quantity * $productBranch->measurement > $product->stock) {
+                        throw new \Exception('Insufficient stock for product ' . $product->name);
+                    }
+        
+                    $product->stock -= $quantity * $productBranch->measurement;
+                    $product->save();
+                }
+        
+                $order = Order::create([
+                    'user_id' => $user_id,
+                    'total_discounted_price' => $discount,
+                    'total_price' => $total,
+                ]);
+        
+                $order->save();
+        
+                if ($order) {
+                    Cart::where('user_id', $user_id)->delete();
+        
+                    DB::commit();
+        
+                    return redirect()->back()->with('message', 'تم اكمال الطلب بنجاح');
+                }
+            } catch (\Exception $e) {
+                DB::rollBack();
+        
+                return redirect()->back()->with('error', 'حدث خطأ أثناء معالجة الطلب. ' . $e->getMessage() . ' يرجى المحاولة مرة أخرى.');
             }
-      $order = Order::create([
-                'user_id' => $user_id,
-                'total_discounted_price' => $discount,
-                'total_price' => $total,
-            ]);
-    
-            $order->save();
-    
-            if ($order) {
-                Cart::where('user_id', $user_id)->delete();
-    
-                DB::commit();
-    
-                return redirect()->back()->with('message', 'تم اكمال الطلب بنجاح');
-            }
-        } catch (\Exception $e) {
-           DB::rollBack();
-    
-            return redirect()->back()->with('message', 'حدث خطأ أثناء معالجة الطلب. يرجى المحاولة مرة أخرى.');
         }
-    }
+        
 }
